583aefe757256cc28076f4c003018b7b75eebf59
Maurício Fonseca <mauricio.lacerdaml@gmail.com>
2021-11-27 14:02:44 -0300

    Criando uma model de lista de negociacoes e utilizando ela para adicionar novar negociacao

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,16 +7,25 @@ contexto do document independente do contexto atual*/
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event) {
         event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao);
+    }
 
-           
-        let negociacao = new Negociacao(
+    _criaNegociacao() {
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
+    }
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor = 0.0;
+        this._inputData.focus();
     }
 }
